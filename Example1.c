7c8ac422c822b50ebe1f687fde85a8bc13bd3cb3
stomaras <sptomaras@yahoo.gr>
2020-02-25 10:44:26 -0500

    Update Example1.c

--- Example1.c
+++ Example1.c
@@ -1,1 +1,21 @@
+/*
+Author: @stomaras
+Date: 25/02/2020
+*/
 
+#include <stdio.h>
+
+int main(){
+
+	char str[100];
+	int i;
+	double x;
+
+	printf("Enter a value:");
+
+	scanf("%d %s", &i, str);
+
+	printf("\nYou entered: %d:::::%s\n", i, str);	
+
+	return 0;
+}
