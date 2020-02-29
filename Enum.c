ea2081d1f87fe4a1436b0b9dfe489b55d5616e00
stomaras <sptomaras@yahoo.gr>
2020-02-29 12:28:40 -0500

    Update Enum.c

--- Enum.c
+++ Enum.c
@@ -14,3 +14,28 @@
 // 2
 // 0
 // 4
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+	// Company is a List 
+	// In the C-Compiler those are represented as integers
+	// So GOOGLE is 0, FACEBOOK is 1, XEROX is 2 e.t.c
+	// Because YAHOO is 10 , EBAY is 11 and MICROSOFT = 12
+	enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO = 10, EBAY, MICROSOFT } ;
+
+	enum Company xerox = XEROX;
+	enum Company google = GOOGLE;
+	enum Company ebay = EBAY;
+
+	printf("The value of xerox is: %d\n", xerox);
+	printf("The value of google is: %d\n", google);
+	printf("The value of ebay is: %d\n", ebay);
+	
+
+	return 0;
+}
+
